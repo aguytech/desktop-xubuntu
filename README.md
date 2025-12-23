8aa8d4b9c845eb70ca848d4aff32e320fe1383ed
aguy tech <aguytech@free.fr>
2025-12-23 09:57:57 +0100

    init from desktop-debian 13.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# desktop-xubuntu
+A script to install and cutomize xubuntu for a desktop usage
---
+++ install.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+######################## CONF
+
+S_TRACE=debug
+_PATH_BASE=$( readlink -f ${0%/*} )
+_PATH_CONF=/usr/local/conf
+_PATH_LOG=/var/log/desktop-install
+_VER_NAME=debian
+
+# inc
+file=${_PATH_BASE}/bs/inc
+! [ -f "${file}" ] && echo "Unable to find file: ${file}" && exit 1
+! . ${file} && echo "Errors while sourcing file: ${file}" && exit 1
+
+########################  WWW
+
+sudo ping -c1 google.com >/dev/null 2>&1 || _exite "Installation needs internet connection"
+
+########################  VARIABLES
+
+export DEBIAN_FRONTEND=noninteractive
+
+########################  PRE
+
+_SPATH=pre
+_source_sub "data" ${_SPATH}
+
+parts_sub="${part_fs} init ssh upgrade global conf end"
+for _PART in ${parts_sub}; do
+	_source_sub "${_PART}" ${_SPATH}
+done
+
+########################  MENU
+
+_SPATH=sub
+parts_install=$( ls ${_PATH_BASE}/${_SPATH} )
+
+while [ "${_PART}" != "quit" ]; do
+	_SDATE=$(date +%s) # renew _SDATE
+	parts_made=" $( grep "^${_SPATH}_" "${S_FILE_INSTALL_DONE}" | cut -d'_' -f2 | xargs ) "
+	parts2do=" "
+	for part in ${parts_install}; do
+		[ "${parts_made/ ${part} }" = "${parts_made}" ] && parts2do+="$part "
+	done
+
+	_echod "parts_made='${parts_made}'"
+	_echod "parts2do='${parts2do}'"
+
+	[ "${parts_made}" ] && _echo "Part already made: ${cyanb}${parts_made}${cclear}"
+	PS3="Give your choice: "
+	select _PART in quit ${parts2do}; do
+		if [ "${parts2do/ ${_PART} /}" != "${parts2do}" ] ; then
+			_source_sub ${_PART} ${_SPATH}
+			break
+		elif [ "${_PART}" = quit ]; then
+			break
+		else
+			_echoe "Wrong option"
+		fi
+	done
+done
+
+_exit
