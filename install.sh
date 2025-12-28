aebb72a5e3b29c8b5c65341322babea83dd96257
aguy tech <aguytech@free.fr>
2025-12-28 12:59:47 +0100

    Modify _BTRFS variable and part

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ export DEBIAN_FRONTEND=noninteractive
 _SPATH=pre
 _source_sub "data" ${_SPATH}
 
-parts_sub="${part_fs} init ssh upgrade global conf end"
+parts_sub="${_BTRFS} init ssh repo upgrade global conf end"
 for _PART in ${parts_sub}; do
 	_source_sub "${_PART}" ${_SPATH}
 done
